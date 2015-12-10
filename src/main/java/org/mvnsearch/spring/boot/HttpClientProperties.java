2bcb7e16f7b5c81ea39813ebc374a34b22450f08
linux_china <linux_china@hotmail.com>
2015-12-10 09:44:10 +0800

    http client properties

---
+++ src/main/java/org/mvnsearch/spring/boot/HttpClientProperties.java
@@ -0,0 +1,74 @@
+package org.mvnsearch.spring.boot;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * HttpClient properties
+ *
+ * @author linux_china
+ */
+@ConfigurationProperties(
+        prefix = "spring.httpclient"
+)
+public class HttpClientProperties {
+    /**
+     * 代理服务器主机名或者ip
+     */
+    private String proxyHost;
+    /**
+     * 代理服务器端口号
+     */
+    private Integer proxyPort;
+    /**
+     * 建立连接超时时间(毫秒): timeout for client to try to connect to the server
+     */
+    private Integer connectTimeout;
+    /**
+     * 等待http响应的超时时间(毫秒): After establishing the connection, timeout for  the client socket to wait for response after sending the request
+     */
+    private Integer socketTimeout;
+    /**
+     * metric name strategy: METHOD_ONLY, HOST_AND_METHOD(default), QUERYLESS_URL_AND_METHOD
+     */
+    private String metricNameStrategy = "HOST_AND_METHOD";
+
+    public String getProxyHost() {
+        return proxyHost;
+    }
+
+    public void setProxyHost(String proxyHost) {
+        this.proxyHost = proxyHost;
+    }
+
+    public Integer getProxyPort() {
+        return proxyPort;
+    }
+
+    public void setProxyPort(Integer proxyPort) {
+        this.proxyPort = proxyPort;
+    }
+
+    public Integer getConnectTimeout() {
+        return connectTimeout;
+    }
+
+    public void setConnectTimeout(Integer connectTimeout) {
+        this.connectTimeout = connectTimeout;
+    }
+
+    public Integer getSocketTimeout() {
+        return socketTimeout;
+    }
+
+    public void setSocketTimeout(Integer socketTimeout) {
+        this.socketTimeout = socketTimeout;
+    }
+
+    public String getMetricNameStrategy() {
+        return metricNameStrategy;
+    }
+
+    public void setMetricNameStrategy(String metricNameStrategy) {
+        this.metricNameStrategy = metricNameStrategy;
+    }
+}
