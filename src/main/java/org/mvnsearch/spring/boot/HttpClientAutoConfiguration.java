403db2e1795149050e485881dabe83bc2ddb4f23
linux_china <linux_china@hotmail.com>
2015-12-10 09:43:34 +0800

    HttpClient auto configuration

---
+++ src/main/java/org/mvnsearch/spring/boot/HttpClientAutoConfiguration.java
@@ -0,0 +1,75 @@
+package org.mvnsearch.spring.boot;
+
+
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.httpclient.HttpClientMetricNameStrategy;
+import com.codahale.metrics.httpclient.InstrumentedHttpClients;
+import org.apache.http.HttpHost;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * HttpClient auto configuration
+ *
+ * @author linux_china
+ */
+@SuppressWarnings("ALL")
+@Configuration
+@ConditionalOnClass(HttpClient.class)
+@ConditionalOnBean(MetricRegistry.class)
+@EnableConfigurationProperties(HttpClientProperties.class)
+public class HttpClientAutoConfiguration {
+    @Autowired
+    private HttpClientProperties properties;
+    @Autowired
+    private MetricRegistry metrics;
+
+    @Bean
+    public RequestConfig httpClientRequestConfig() {
+        RequestConfig.Builder requestConfigBuilder = RequestConfig.custom();
+        if (properties.getConnectTimeout() != null) {
+            requestConfigBuilder.setConnectionRequestTimeout(properties.getConnectTimeout());
+        }
+        if (properties.getSocketTimeout() != null) {
+            requestConfigBuilder.setSocketTimeout(properties.getSocketTimeout());
+        }
+        if (properties.getProxyHost() != null) {
+            requestConfigBuilder.setProxy(new HttpHost(properties.getProxyHost(), properties.getProxyPort()));
+        }
+        return requestConfigBuilder.build();
+    }
+
+    @Bean
+    public HttpClient httpClient(RequestConfig requestConfig) {
+        HttpClientMetricNameStrategy nameStrategy = HttpClientMetricNameStrategies.HOST_AND_METHOD;
+        if (properties.getMetricNameStrategy() != null) {
+            if (properties.getMetricNameStrategy().equalsIgnoreCase("QUERYLESS_URL_AND_METHOD")) {
+                nameStrategy = HttpClientMetricNameStrategies.QUERYLESS_URL_AND_METHOD;
+            } else if (properties.getMetricNameStrategy().equalsIgnoreCase("METHOD_ONLY")) {
+                nameStrategy = HttpClientMetricNameStrategies.METHOD_ONLY;
+            }
+        }
+        HttpClientBuilder clientBuilder = InstrumentedHttpClients.custom(metrics, nameStrategy);
+        clientBuilder.setDefaultRequestConfig(requestConfig);
+        return clientBuilder.build();
+    }
+
+    @Bean
+    public Executor httpClientExecutor(HttpClient httpClient) {
+        return Executor.newInstance(httpClient);
+    }
+
+    @Bean
+    public HttpClientEndpoint httpClientEndpoint(HttpClientProperties properties, MetricRegistry metrics) {
+        return new HttpClientEndpoint(properties, metrics);
+    }
+}
